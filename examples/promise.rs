1195f78e07e5d58eb2fcf842c5ff4f4bd51ff552
Y. T. Chung <zonyitoo@gmail.com>
2015-10-05 23:46:28 +0800

    better looking

--- examples/promise.rs
+++ examples/promise.rs
@@ -15,19 +15,19 @@ fn main() {
                         Err("Final error")
                     }
                 })
-                            .then(|res| {
-                                      assert_eq!(res, 1.23);
-                                      Ok(34)
-                                  },
-                                  |err| {
-                                      assert_eq!(err, "Final error");
-                                      if true {
-                                          Ok(35)
-                                      } else {
-                                          Err(44u64)
-                                      }
-                                  })
-                            .sync();
+                .then(|res| {
+                          assert_eq!(res, 1.23);
+                          Ok(34)
+                      },
+                      |err| {
+                          assert_eq!(err, "Final error");
+                          if true {
+                              Ok(35)
+                          } else {
+                              Err(44u64)
+                          }
+                      })
+                .sync();
 
                 assert_eq!(r, Ok(34));
             });
