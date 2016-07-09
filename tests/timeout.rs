83be405c000484a9088403d60e5e21cc9cc49041
Y. T. Chung <zonyitoo@gmail.com>
2016-07-09 20:43:09 +0800

    add logs

--- tests/timeout.rs
+++ tests/timeout.rs
@@ -1,4 +1,5 @@
 extern crate coio;
+extern crate env_logger;
 
 use std::io::{Read, Write};
 use std::time::Duration;
@@ -9,13 +10,18 @@ use coio::sleep;
 
 #[test]
 fn test_tcp_echo() {
+    env_logger::init().unwrap();
+
     Scheduler::new()
         .run(move || {
             let acceptor = TcpListener::bind("127.0.0.1:6789").unwrap();
 
             // Listener
             let listen_fut = Scheduler::spawn(move || {
-                let (mut stream, _) = acceptor.accept().unwrap();
+                println!("LISTEN: Started");
+
+                let (mut stream, addr) = acceptor.accept().unwrap();
+                println!("LISTEN: Accepted {:?}", addr);
 
                 let mut buf = [0u8; 1024];
                 while let Ok(len) = stream.read(&mut buf) {
@@ -24,21 +30,31 @@ fn test_tcp_echo() {
                         break;
                     }
 
+                    println!("LISTEN: Going to sleep");
                     sleep(Duration::from_secs(1));
                     break;
                 }
+
+                println!("LISTEN: Finished");
             });
 
             let sender_fut = Scheduler::spawn(move || {
+                println!("SEND: Started");
+
                 let mut stream = TcpStream::connect("127.0.0.1:6789").unwrap();
                 stream.write_all(b"abcdefg")
-                      .and_then(|_| stream.flush())
-                      .unwrap();
+                    .and_then(|_| stream.flush())
+                    .unwrap();
+
+                println!("SEND: Written");
 
                 let _ = stream.set_read_timeout(Some(Duration::from_millis(100)));
 
+                println!("SEND: Waiting to read");
                 let mut buf = [0u8; 1024];
                 assert!(stream.read(&mut buf).is_err());
+
+                println!("SEND: Finished");
             });
 
             listen_fut.join().unwrap();
